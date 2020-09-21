f60c53a65302e70f70a96e528109b90b1705fa9b
jkmit <1793844527@qq.com>
2020-09-21 09:42:07 +0800

    '添加数据迁移功能'

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask
-
+from flask_migrate import Migrate,MigrateCommand
+from flask_script import Manager
 from views import index_blu
 from models import db
 
@@ -13,6 +14,13 @@ app = Flask(__name__)
 app.config.from_pyfile('config.ini')
 # 注册蓝图
 app.register_blueprint(index_blu)
+
+# 添加数据库迁移工具
+manager = Manager(app)
+#生成 migrate 对象，用来迁移数据库
+migrate = Migrate(manager, db)
+# 添加db 命令
+manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     app.run(port=7788)
