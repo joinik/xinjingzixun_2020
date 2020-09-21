c98fe27de939dc179c19074bd786a851e4cbdb0c
jkmit <1793844527@qq.com>
2020-09-21 09:18:38 +0800

    '添加templates文件'

--- views/index.py
+++ views/index.py
@@ -1,8 +1,10 @@
 from . import index_blu
+from flask import render_template
 
 
 
 
 @index_blu.route('/')
 def index():
-	return "主页index ----"
+	# return "主页index ----"
+	return render_template('index.html')
