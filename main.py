e37b2cdb79c70dae05399295eedd13120f89b3b6
jkmit <1793844527@qq.com>
2020-09-21 09:34:42 +0800

    '加载flask配置信息'

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from flask import Flask
 
 from views import index_blu
+from models import db
+
 
 
 
 # 创建App对象
 app = Flask(__name__)
 
+# 加载配配信息
+app.config.from_pyfile('config.ini')
 # 注册蓝图
 app.register_blueprint(index_blu)
 
