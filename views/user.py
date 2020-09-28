ff2c24e4fc9ef859f55cfc3acb4e535adbd0b899
jkmit <1793844527@qq.com>
2020-09-28 09:03:55 +0800

    ''

--- views/user.py
+++ views/user.py
@@ -1,5 +1,3 @@
-
-
 from flask import jsonify, request, session, render_template
 
 from models import db
@@ -7,7 +5,7 @@ from models.index import User, Follow
 from . import user_blu
 
 
-@user_blu.route("/user/follow", methods=["POST"])
+@user_blu.route ("/user/follow", methods=["POST"])
 def follow():
 	# 实现关注的流程
 	# 1. 提取当前作者的id
@@ -16,40 +14,37 @@ def follow():
 	# 4. 如果未关注，则进行关注
 
 	# 根据action 的不同来判断是否关注
-	news_action = request.json.get("action")
+	news_action = request.json.get ("action")
 	# 1. 提取当前作者的id
 	news_author_id = request.json.get ("user_id")
-	print(news_author_id, "------->1")
+	print (news_author_id, "------->1")
 	# 2. 提取当前登录用户的id
 	user_id = session.get ("user_id")
-	print(user_id, "------->2")
-
+	print (user_id, "------->2")
 
 	if news_action == "do":
 
 		# 3. 判断之前是否已经关注过
-		news_author = db.session.query(User).filter(User.id == news_author_id).first()
+		news_author = db.session.query (User).filter (User.id == news_author_id).first ()
 
 		if user_id in [x.id for x in news_author.followers]:
-			print("3001-----------------------已经关注")
-			return jsonify({
-				"errno":3001,
-				"errmsg":"已经关注了，请勿重复关注"
+			print ("3001-----------------------已经关注")
+			return jsonify ({
+				"errno": 3001,
+				"errmsg": "已经关注了，请勿重复关注"
 			})
-
-
 
 		# 4. 如果未关注，则进行关注
 		try:
-			follow = Follow(followed_id=news_author_id, follower_id=user_id)
-			db.session.add(follow)
-			db.session.commit()
+			follow = Follow (followed_id=news_author_id, follower_id=user_id)
+			db.session.add (follow)
+			db.session.commit ()
 			ret = {
 				"errno": 0,
 				"errmsg": "关注成功"
 			}
 
-			return jsonify(ret)
+			return jsonify (ret)
 
 
 		except Exception as ret:
@@ -57,7 +52,7 @@ def follow():
 				"errno": 3003,
 				"errmsg": "关注失败"
 			}
-			print("""-----------+++++++try--""")
+			print ("""-----------+++++++try--""")
 			return jsonify (ret)
 
 	else:
@@ -84,8 +79,11 @@ def follow():
 			return jsonify (ret)
 
 
-@user_blu.route("/user/center")
+@user_blu.route ("/user/center")
 def user_center():
-    return render_template("user.html")
+	return render_template ("user.html")
 
 
+@user_blu.route ("/user/user_base_info.html")
+def user_base_info():
+	return render_template ("user_base_info.html")
