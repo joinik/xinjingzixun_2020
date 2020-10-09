303421324e3bfa192fc09c2d97d9c9265dfd905e
jkmit <1793844527@qq.com>
2020-10-09 08:36:39 +0800

    '146-【新闻详情页】添加评论实现'

--- views/news.py
+++ views/news.py
@@ -1,12 +1,12 @@
 from flask import jsonify, request, session
 
 from models import db
-from models.index import User, Collection
+from models.index import User, Collection, Comment
 
 from . import news_blu
 
 
-@news_blu.route("/news/collect", methods=["POST"])
+@news_blu.route ("/news/collect", methods=["POST"])
 def news_collect():
 	# 1. 提取参数
 	news_id = request.json.get ("news_id")
@@ -49,4 +49,26 @@ def news_collect():
 				"errmsg": "取消收藏失败"
 			}
 
+	return jsonify (ret)
+
+
+@news_blu.route ("/news/comment", methods=["POST"])
+def news_comment():
+	# 1. 提取出用户评价时的数据
+	content = request.json.get ("content")
+	news_id = request.json.get ("news_id")
+	user_id = session.get ("user_id")
+
+	# 2. 保存到数据库
+	new_comment = Comment ()
+	new_comment.news_id = news_id
+	new_comment.user_id = user_id
+	new_comment.content = content
+	db.session.add (new_comment)
+	db.session.commit ()
+
+	ret = {
+		"errno": 0,
+		"errmsg": "成功"
+	}
 	return jsonify (ret)
