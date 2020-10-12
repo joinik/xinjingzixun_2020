9672a8f938223406dfa992d55f1ebaee355494b6
joinik <1793844527@qq.com>
2020-10-12 08:54:59 +0800

    '165-【后台】创建新蓝图，显示后台测试页面'

--- views/__init__.py
+++ views/__init__.py
@@ -4,7 +4,11 @@ index_blu = Blueprint ("index_blu", __name__)
 passport_blu = Blueprint("passport_blu", __name__)
 user_blu = Blueprint("user_blu", __name__)
 news_blu = Blueprint("news_blu", __name__)
+admin_blu = Blueprint("admin_blu", __name__)
+
+
 from . import index
 from . import passport
 from . import user
 from . import news
+from . import admin
